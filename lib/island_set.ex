aff79dbad6190701d0d2da84217de456a805df3e
Sean Clayton <sean@seanclayton.me>
2017-04-22 09:28:15 -0400

    Add IslandSet module

---
+++ lib/island_set.ex
@@ -0,0 +1,22 @@
+defmodule IslandFury.IslandSet do
+  alias IslandFury.{Island, IslandSet}
+
+  defstruct atoll: :none, dot: :none, l_shape: :none, s_shape: :none, square: :none
+
+  def start_link() do
+    Agent.start_link(fn -> initialized_set() end)
+  end
+
+  def initialized_set() do
+    Enum.reduce(keys(), %IslandSet{}, fn key, set ->
+      {:ok, island} = Island.start_link
+      Map.put(set, key, island)
+    end)
+  end
+
+  def keys() do
+    %IslandSet{}
+    |> Map.from_struct
+    |> Map.keys
+  end
+end
